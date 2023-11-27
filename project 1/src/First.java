eef641c5277a6d316bf34541592085aee0e59410
sergey <sergeysereko@gmail.com>
2023-11-27 16:56:51 +0200

    add first task

---
+++ project 1/src/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("“Your time is limited,\n" +
+                "\tso don’t waste it\n" +
+                "\t\tliving someone else’s life”\n" +
+                "\t\t\tSteve Jobs");
+    }
+}
